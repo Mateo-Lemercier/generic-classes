e7b8062065206d2b2f3131078d8b8c5f7fdf5ba6
Mateo Lemercier <mateolemercier15@yahoo.com>
2024-11-09 22:46:50 +0100

    [ADD] SinglyCircularLinkedList

---
+++ src/Containers/LinkedLists/SinglyCircularLinkedList.hpp
@@ -0,0 +1,19 @@
+#ifndef SINGLYCIRCULARLINKEDLIST_HPP_INCLUDED
+#define SINGLYCIRCULARLINKEDLIST_HPP_INCLUDED
+
+
+
+template <typename T>
+class SinglyCircularLinkedList
+{
+public:
+    SinglyCircularLinkedList() = default;
+    ~SinglyCircularLinkedList() = default;
+
+protected:
+    unsigned int m_size = 0;
+};
+
+
+
+#endif
