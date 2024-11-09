d31be723ba69dc4b624758d93a857c0dede0960e
Mateo Lemercier <mateolemercier15@yahoo.com>
2024-11-10 00:46:25 +0100

    [FIX] SCLL Tests header

--- src/Containers/LinkedLists/SinglyCircularLinkedList-tests.h
+++ src/Containers/LinkedLists/SinglyCircularLinkedList-tests.h
@@ -1,8 +1,3 @@
 #pragma once
-#include <ostream>
-
-std::ostream& PushFront( std::ostream& os );
-std::ostream& PushBack( std::ostream& os );
-std::ostream& PushAfter( std::ostream& os );
 
 void SinglyCircularLinkedListTests();
