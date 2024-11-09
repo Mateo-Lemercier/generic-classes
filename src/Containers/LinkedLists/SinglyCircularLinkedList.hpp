5f7725c001a6d33cef117128f669426d106e0640
Mateo Lemercier <mateolemercier15@yahoo.com>
2024-11-09 23:07:28 +0100

    [ADD] SCLL Node

--- src/Containers/LinkedLists/SinglyCircularLinkedList.hpp
+++ src/Containers/LinkedLists/SinglyCircularLinkedList.hpp
@@ -7,12 +7,40 @@ template <typename T>
 class SinglyCircularLinkedList
 {
 public:
+    class Node;
+
     SinglyCircularLinkedList() = default;
     ~SinglyCircularLinkedList() = default;
 
 protected:
     unsigned int m_size = 0;
+    Node* m_pTail = nullptr;
 };
+
+
+
+template <typename T>
+class SinglyCircularLinkedList<T>::Node
+{
+protected:
+    explicit Node( T const& data );
+
+    T m_data;
+    Node* m_pNext = this;
+
+    friend class SinglyCircularLinkedList;
+};
+
+
+
+#pragma region Node
+
+
+template <typename T>
+SinglyCircularLinkedList<T>::Node::Node( T const& data ) : m_data( data ) {}
+
+
+#pragma endregion
 
 
 
