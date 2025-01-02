2a78db33f1d6c7bc3dc59991680cc5b3158db5c8
Mateo Lemercier <mateolemercier15@yahoo.com>
2025-01-02 09:39:09 +0100

    [ADD] Vectors.h

--- src/Math.h
+++ src/Math.h
@@ -2,7 +2,7 @@
 #define MATH_H_INCLUDED
 
 
-
+#include "Math/Vectors.h"
 
 
 #endif
---
+++ src/Math/Vectors.h
@@ -0,0 +1,10 @@
+#ifndef MATH_VECTOR_H_INCLUDED
+#define MATH_VECTOR_H_INCLUDED
+
+
+#include "Vector.hpp"
+#include "Vector2.hpp"
+#include "Vector3.hpp"
+
+
+#endif
