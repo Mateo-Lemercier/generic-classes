5bba383ffe891cb0db328ed26521065bac41cb1a
Mateo Lemercier <mateolemercier15@yahoo.com>
2025-02-06 16:44:59 +0100

    [REF] define type names

--- src/define.h
+++ src/define.h
@@ -4,5 +4,18 @@
 
 #define assert( condition ) if ( condition == false ) throw;
 
+using int8 = char;
+using int16 = short;
+using int32 = int;
+using int64 = long long;
+
+using uint8 = unsigned char;
+using uint16 = unsigned short;
+using uint32 = unsigned int;
+using uint64 = unsigned long long;
+
+using float32 = float;
+using float64 = double;
+
 
 #endif
